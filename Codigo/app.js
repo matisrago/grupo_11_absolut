1fc22772f591efccef446f4d0f46b970559576f6
juulioe <julio.eze.1999@gmail.com>
2021-09-28 14:13:02 -0300

    Detalle primer version

--- Codigo/app.js
+++ Codigo/app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 var path = require('path');
 const port = 3030
+app.use(express.static("public"));
 app.listen(port, () => {
     console.log("Funcionando")
+})
+app.get("/detalle",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/detalleProducto.html"))
 })
