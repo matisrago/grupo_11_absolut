7b00fb2184e272063435b6aa04086762cafe0099
juulioe <julio.eze.1999@gmail.com>
2021-11-29 14:30:22 -0300

    Login v2

--- Codigo/middlewares/loginMiddleware.js
+++ Codigo/middlewares/loginMiddleware.js
@@ -1,3 +1,4 @@
+const { RSA_NO_PADDING } = require("constants");
 const fs = require("fs");
 const path = require("path")
 const userFilePath = path.join(__dirname, '../data/users.json');
@@ -6,14 +7,14 @@ const users = JSON.parse(fs.readFileSync(userFilePath, 'utf-8'));
 function loginMiddleWare(req , res ,next){
     let usuarioEmail = req.body.usuario;
     let usuarioPassword = req.body.contraseña
-    for(let i = 0; i<users.length ; i++){
-
-        if (users[i].email == usuarioEmail && users[i].password == usuarioPassword){
+    for(var i = 0; i<users.length ; i++){
+        if(users[i].email == usuarioEmail && users[i].password === usuarioPassword){
             next()
-        }else{
-            res.render("formDeLogin")
         }
+     
     }
+    
+    res.render("formDeLogin")
 }
 
 module.exports = loginMiddleWare
