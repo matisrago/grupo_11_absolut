3356a032c62994bf01c9606ea6f6c8a84bda9169
juulioe <julio.eze.1999@gmail.com>
2021-12-07 21:14:01 -0300

    Error login solucionado

--- Codigo/controllers/userControllers.js
+++ Codigo/controllers/userControllers.js
@@ -1,6 +1,7 @@
 const { validationResult } = require('express-validator');
 const fs = require('fs');
 const path = require('path');
+const bcrypt = require("bcryptjs")
 
 const productsFilePath = path.join(__dirname, '../data/users.json');
 const users = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
@@ -13,22 +14,21 @@ const usersControllers = {
         let errors = validationResult(req);
         if(errors.isEmpty()){
             for(let i = 0 ; i< users.length ; i++){
-                if(users[i].email === req.body.usuario && users[i].password === req.body.password){
-                        var usuarioALoguearse = users[i];
-                        
-                        
+                let userPassword  = req.body.password
+                if(users[i].email == req.body.usuario && users[i].password == req.body.password){
+                    var usuarioALoguearse = users[i]
                 }
             }
             if(usuarioALoguearse == undefined){
                 return res.render("formDeLogin",{errors:[{msg:"Credenciales invalidas"}]})
             }
-            req.session.usuarioLogueado = usuarioALoguearse
-                res.redirect("/")
-
+            
         }else{
             return res.render('formDeLogin',{errors:errors.errors})
         }
-
+        
+        req.session.usuarioLogueado = usuarioALoguearse
+            res.redirect("/")
     },
     formulario:(req,res)=>{
         res.render("formDeRegistro")
