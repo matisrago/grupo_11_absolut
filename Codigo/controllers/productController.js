3a18473c76547dac58d567748f2e3c35fb52d9e6
matisrago <matiassragowicz@hotmail.com>
2022-01-10 03:22:25 -0300

    Cambie diseño del home y el detalle y termine el update de los productos

--- Codigo/controllers/productController.js
+++ Codigo/controllers/productController.js
@@ -1,38 +1,47 @@
 const fs = require('fs');
 const path = require('path');
 
-const productsFilePath = path.join(__dirname, '../data/products.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 let db = require("../database/models")
 
 const productController = {
 	detalle: (req, res) => {
-		db.Products.findByPk(req.params.id)
-		.then((product)=>{res.render('detalleProducto',{product,user :req.session.usuarioLogueado})})
+		db.Products.findAll()
+		.then((products)=>{db.Products.findByPk(req.params.id)
+			.then((product)=>{res.render('detalleProducto',{product,products,user :req.session.usuarioLogueado})})})
+		
 	},
 	crear: (req,res ) => {
 		
 		res.render('creacionProducto',{user :req.session.usuarioLogueado})
 	},
 	editar: (req,res)=> {
-		const productsDetails = products.find(prod=> prod.id == req.params.id)
-		res.render('edicionProducto',{productsDetails,user :req.session.usuarioLogueado})
+		db.Products.findByPk(req.params.id)
+		.then((product)=>{res.render('edicionProducto',{product,user :req.session.usuarioLogueado})})
+		
 	},
 	agregar: (req,res ) => {
-		
-		const productId = req.params.id;
-
-		const productIndex = products.findIndex((p) => p.id == productId);
-
-		const updatedProduct = {
-			...products[productIndex],
-			...req.body,
-			price: Number (req.body.price),
-			discount: Number (req.body.discount),
-			
-		};
-		products[productIndex] = updatedProduct;
-		fs.writeFileSync(-productsFilePath,JSON.stringify(products, null," "));
+		let ubicacionCategoria = null
+		if(req.body.category === "oferta"){
+			ubicacionCategoria = 1	
+		}else{
+			ubicacionCategoria = 2
+		}
+		let productoAEditar = req.params.id
+		let nombreEditado = req.body.name
+		let descripcionEditada = req.body.description
+		let precioEditado = req.body.price
+		let categoriaEditada = ubicacionCategoria
+		db.Products.findOne({
+			where: [{ id: productoAEditar}]
+		})
+		.then((product)=>{
+			product.update({
+				name:nombreEditado,
+				description:descripcionEditada,
+				price:precioEditado,
+				id_ubicacion:categoriaEditada
+			})
+		})
 
 		res.redirect('/')
 	},
