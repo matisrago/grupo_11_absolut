6ba906329c3b3537dace80c6bf7363f8b85dc39c
TomasButelerT <tomybute@gmail.com>
2022-02-07 22:09:14 -0300

    Descomitie algunas validaciones y subi imagenes

--- Codigo/database/config/config.js
+++ Codigo/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports={
   "development": {
     "username": "root",
-    "password": "rojo2008",
+    "password": null,
     "database": "structure.sql",
     "host": "127.0.0.1",
     "dialect": "mysql"
