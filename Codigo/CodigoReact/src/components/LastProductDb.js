fc51a041b91d32291010a68a38fab0065c97ccd5
TomasButelerT <tomybute@gmail.com>
2022-02-09 22:59:38 -0300

    Modifique el css y el html del dashBoard SideBAr

--- Codigo/CodigoReact/src/components/LastProductDb.js
+++ Codigo/CodigoReact/src/components/LastProductDb.js
@@ -8,11 +8,16 @@ function lastProductDb(props){
 								<div className="card-header py-3">
 									<h5 className="m-0 font-weight-bold text-gray-800">Last product in Data Base</h5>
 								</div>
+								
 								<div className="card-body">
 									<div className="text-center">
 									
 									</div>
 									<p>{props.ultimoProducto.name}</p>
+								</div>
+								<div>
+									<img style={{width: '70%'}}src={`${props.ultimoProducto.image}`}/>
+									
 								</div>
 							</div>
 						</div>
